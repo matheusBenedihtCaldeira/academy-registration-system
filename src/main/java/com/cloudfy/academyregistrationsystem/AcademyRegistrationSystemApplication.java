c434006dfb360dff2b01758697f257ecf2e086a8
bene <matheusbenediht@gmail.com>
2023-08-08 17:18:37 -0300

    First commit

---
+++ src/main/java/com/cloudfy/academyregistrationsystem/AcademyRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cloudfy.academyregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyRegistrationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyRegistrationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudfy/academyregistrationsystem/AcademyRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudfy.academyregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
