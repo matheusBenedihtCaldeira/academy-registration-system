d0662aac0fd18725ffe9f20b7600761a80af5074
bene <matheusbenediht@gmail.com>
2023-08-10 11:14:11 -0300

    Update operator

---
+++ src/main/java/com/cloudfy/academyregistrationsystem/controllers/operator/UpdateOperatorCotroller.java
@@ -0,0 +1,26 @@
+package com.cloudfy.academyregistrationsystem.controllers.operator;
+
+import com.cloudfy.academyregistrationsystem.models.dto.OperatorDTO;
+import com.cloudfy.academyregistrationsystem.models.entities.Operator;
+import com.cloudfy.academyregistrationsystem.services.operator.UpdateOperatorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RestController
+@RequestMapping(value = "/update/operator")
+public class UpdateOperatorCotroller {
+
+    @Autowired
+    private UpdateOperatorService service;
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Operator> update(@RequestBody OperatorDTO data, @PathVariable Long id){
+        Operator operator = service.fromDTO(data);
+        operator.setId(id);
+        service.update(operator);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/cloudfy/academyregistrationsystem/services/operator/UpdateOperatorService.java
@@ -0,0 +1,37 @@
+package com.cloudfy.academyregistrationsystem.services.operator;
+
+import com.cloudfy.academyregistrationsystem.models.dto.OperatorDTO;
+import com.cloudfy.academyregistrationsystem.models.entities.Operator;
+import com.cloudfy.academyregistrationsystem.repositories.customer.OperatorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateOperatorService {
+
+    @Autowired
+    private OperatorRepository repository;
+
+    public Operator update(Operator dataToUpdate){
+        Operator operatorFromDB = findById(dataToUpdate.getId());
+        updateData(operatorFromDB, dataToUpdate);
+        return repository.save(operatorFromDB);
+    }
+
+    public void updateData(Operator operatorFromDB, Operator dataToUpdate){
+        operatorFromDB.setName(dataToUpdate.getName());
+        operatorFromDB.setEmail(dataToUpdate.getEmail());
+        operatorFromDB.setOperatorKey(dataToUpdate.getOperatorKey());
+        operatorFromDB.setPassword(dataToUpdate.getPassword());
+    }
+
+    public Operator findById(Long id){
+        Operator operator = repository.findById(id).get();
+        return operator;
+    }
+
+    public Operator fromDTO(OperatorDTO data){
+        return new Operator(data.getId(), data.getName(),data.getEmail(),data.getOperatorKey(),data.getPassword());
+    }
+
+}
